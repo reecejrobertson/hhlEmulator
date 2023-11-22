288559fda5dd7c64d52c6deec52a555e34361f34
Reece J Robertson <reecejrobertson@pop-os.localdomain>
2023-11-22 13:04:46 -0500

    First major update.

--- comparison.py
+++ comparison.py
@@ -1,23 +1,27 @@
 import numpy as np
+import emulator as em
+import simulator as sim
 
-u0 = np.array([-1/np.sqrt(2), -1/np.sqrt(2)], dtype=float)
-u1 = np.array([-1/np.sqrt(2), 1/np.sqrt(2)], dtype=float)
-l0 = 2/3.
-l1 = 4/3.
-b0 = -1/np.sqrt(2)
-b1 = 1/np.sqrt(2)
-c = 2/3.
+def runExperiment(A, b, shots=2048, emFig='emulator.png', simFig='simulator.png'):
+    print('Begin Experiment')
+    print('A=['
+        + np.array_str(A[0], precision=2,) + ','
+        + np.array_str(A[1], precision=2) + '], b='
+        + np.array_str(b, precision=2)
+    )
+    x = np.linalg.solve(A, b)
+    actual = (x / np.linalg.norm(x))**2
+    print('Actual x\t:', actual)
+    simEstimate, simTime = sim.simulateHHL(A, b, numQubits=4, shots=shots, figfile=simFig)
+    print('Simulated x\t:', simEstimate)
+    emEstimate, emTime = em.emulateHHL(A, b, shots=shots, figfile=emFig)
+    print('Emulated x\t:', emEstimate)
+    print('Sim Time\t:', simTime)
+    print('Em Time \t:', emTime)
+    print('Sim Avg Time\t:', simTime/shots)
+    print('Em Avg Time\t:', emTime/shots)
+    print('End Experiment')
+    print()
 
-a0 = np.array([np.sqrt(1 - c**2/l0**2), c/l0])
-a1 = np.array([np.sqrt(1 - c**2/l1**2), c/l1])
-psi = np.kron(b0 * u0, a0) + np.kron(b1 * u1, a1)
-
-# x = np.array([
-#     (1/2) * np.sqrt(7/16) - (1/2) * np.sqrt(55/64),
-#     3/8 - 3/16,
-#     (1/2) * np.sqrt(7/16) + (1/2) * np.sqrt(55/64),
-#     3/8 + 3/16,
-# ], dtype=float)
-
-print(psi)
-print(psi**2)
+runExperiment(np.array([[1 , -1/3], [-1/3 , 1]]), np.array([0, 1]), emFig='em1.png', simFig='sim1.png')
+runExperiment(np.array([[5/2., -1/2.], [-1/2., 5/2]]), np.array([0, 1]), emFig='em2.png', simFig='sim2.png')
